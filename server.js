ad36dfeef2c5b0e0453c0b5d8a6098b8bc17904a
Matthew Tregear <matthewtregear@Matthews-MacBook-Pro.local>
2018-10-15 17:25:12 +0100

    fixed again

--- server.js
+++ server.js
@@ -2,84 +2,55 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
-const port = process.env.PORT || 3000;
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 
-hbs.registerPartials(__dirname +'/views/partials')
-app.set('view engine','hbs')
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
 
-app.use((req,res,next) => {
-	var now = new Date().toString();
-
-	var log = `${now}: ${req.method} ${req.url}`;
-	console.log(log);
-	fs.appendFile('server.log', log + '\n', (err) =>{
-		if (err){
-			console.log('Unable to append to server', log + '\n')
-		}
-	})
-
-
-
-
-	next();
-
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
 });
 
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
-app.listen(port, () =>{
-	console.log(`Server is up on port ${port}`);
-});
-
-app.use((req,res,next) => {
-
-res.render('maintenance.hbs');
-
-
-
-
-});
-
-app.use(express.static(__dirname +'/public'));
-
-
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-	return new Date().getFullYear();
+  return new Date().getFullYear();
 });
 
-hbs.registerHelper('screamIt', (text) =>{
-	return text.toUpperCase();
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
 });
 
-app.get('/',(req,res) =>{
-	//res.send('<h1>Hello Express!</h1>');
-
-	res.render('home.hbs',{
-		pageTitle: 'HomePage',
-		currentYear: new Date().getFullYear(),
-		welcomeMessage: 'Welcome to the homepage'
-	});
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
 });
 
-
-
-app.get('/about',(req,res) => {
-	res.render('about.hbs',{
-		pageTitle: 'About Page',
-		currentYear: new Date().getFullYear()
-	});
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
 
-
-
-
-//
-app.get('/bad', (req,res) =>{
-
-	res.send({
-		errormessage: 'Unable to handle request'
-	})
-})
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
